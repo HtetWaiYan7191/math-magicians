5ad4a4e584b9ae80cc07f4e49239bab0fbbe8f91
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-03 19:33:42 +0630

    Create a new child component for each button and style file for that component

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './Buttons.css';
 import PropTypes from 'prop-types';
 
-const Buttons = ({ number }) => (
-    number === '0' ? <div className='input-button zero'>{number}</div> :<div className='input-button'>{number}</div>
-);
+const Buttons = ({ number }) => {
+  if (number === '\u00F7' || number === 'x' || number === '-' || number === '+' || number === '=') {
+    return <div className="input-button orange">{number}</div>;
+  } if (number === '0') {
+    return <div className="input-button zero">{number}</div>;
+  }
+  return <div className="input-button">{number}</div>;
+};
 
 Buttons.propTypes = {
-  number: PropTypes.isRequired,
+  number: PropTypes.number.isRequired,
 };
 
 export default Buttons;
