828bbd40ba6de2a42abf5ed48fdc49558fdce962
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-03 19:34:37 +0630

    Add Calculator component container and Calculator.css file to style that component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Calculator.css';
 import Buttons from './Buttons';
 
-const numbers = ['AC', '+/-', '%', '\u00F7', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '='];
+const numbers = ['AC', '+/-', '%', '\u00F7', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 const Calculator = () => (
   <div className="container">
     <div className="output-result">0</div>
