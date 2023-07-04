ca9641c8663a4ac2eb8c8eeda70c5e9df22b1ff7
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-04 10:27:50 +0630

    Get event handler function from parent and use it

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './Buttons.css';
 import PropTypes from 'prop-types';
 
-const Buttons = ({ number }) => {
+const Buttons = ({ number, handleInput }) => {
   if (number === '\u00F7' || number === 'x' || number === '-' || number === '+' || number === '=') {
-    return <button type="button" className="input-button orange">{number}</button>;
+    return <button type="button" onClick={handleInput} className="input-button orange">{number}</button>;
   } if (number === '0') {
-    return <button type="button" className="input-button zero">{number}</button>;
+    return <button type="button" onClick={handleInput} className="input-button zero">{number}</button>;
   }
-  return <button type="button" className="input-button">{number}</button>;
+  return <button type="button" onClick={handleInput} className="input-button">{number}</button>;
 };
 
 Buttons.propTypes = {
   number: PropTypes.number.isRequired,
+  handleInput: PropTypes.func.isRequired,
 };
 
 export default Buttons;
