04bb117216e8c600f5643da278630cfdd85951f8
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-10 21:41:50 +0630

    Implement test function for Quote.js

---
+++ src/__test__/Quote.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import Quote from '../components/Quote';
+
+describe('Quote', () => {
+    test('should match the snapShot', () => {
+        const component = renderer.create(<Quote/>);
+        const instance = component.toJSON();
+        expect(instance).toMatchSnapshot();
+    })
+})
