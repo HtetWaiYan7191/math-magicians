cc1bc1e62e93360a8a8797733c90f5182de36806
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-10 20:40:37 +0630

    Add test for calculate.js

---
+++ src/logic/calculate.test.js
@@ -0,0 +1,22 @@
+import calculate from "./calculate";
+
+describe('calculate',() => {
+    test('should handle AC button', () => {
+        const result = calculate({total:'10', next:'5', operation: '+'}, 'AC');
+        expect(result).toEqual({
+            total:null,
+            next:null,
+            operation:null,
+        });
+    });
+
+    test('should handle number buttons', () => {
+        const result2 = calculate({total: '10', next:'5', operation: '+'}, '1');
+        expect(result2).toEqual({ total: '10', next: '51', operation: '+' });
+    })
+
+    test('should perform addition correctly', () => {
+        const result3 = calculate({total: '5', next:'4', operation: '+'}, '=');
+        expect(result3).toEqual({total:'9', next:null, operation:null});
+    })
+})
